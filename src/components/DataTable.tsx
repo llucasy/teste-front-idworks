b64be769fc8da76be5025dcaf8623817d9961dce
Lucas ROdrigues <llucasy@hotmail.com>
2025-01-12 23:33:18 -0300

    delete function

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -1,4 +1,4 @@
-import { Space, Table } from 'antd'
+import { Button, Space, Table } from 'antd'
 import type { TableProps } from 'antd'
 
 interface DataType {
@@ -8,39 +8,42 @@ interface DataType {
   location: string
 }
 
-const columns: TableProps<DataType>['columns'] = [
-  {
-    title: 'ID',
-    dataIndex: 'id',
-    key: 'id',
-  },
-  {
-    title: 'Nome',
-    dataIndex: 'name',
-    key: 'name',
-  },
-  {
-    title: 'Quantidade',
-    dataIndex: 'quantity',
-    key: 'quantity',
-  },
-  {
-    title: 'Localização',
-    dataIndex: 'location',
-    key: 'location',
-  },
-  {
-    title: 'Ação',
-    key: 'action',
-    render: (_, record) => (
-      <Space size="middle">
-        <a>Deletar</a>
-      </Space>
-    ),
-  },
-]
+export default function DataTable({ data, onDelete }: { data: DataType[], onDelete: (id: string) => void }) {
 
-export default function DataTable({ data }: { data: DataType[] }) {
+  const columns: TableProps<DataType>['columns'] = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      key: 'id',
+    },
+    {
+      title: 'Nome',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Quantidade',
+      dataIndex: 'quantity',
+      key: 'quantity',
+    },
+    {
+      title: 'Localização',
+      dataIndex: 'location',
+      key: 'location',
+    },
+    {
+      title: 'Ação',
+      key: 'action',
+      render: (_, record) => (
+        <Space size="middle">
+          <Button onClick={() => onDelete(record.id)} size="small">
+            Deletar
+          </Button>
+        </Space>
+      ),
+    },
+  ]
+
   return (
     <Table<DataType>
       columns={columns}
@@ -48,7 +51,7 @@ export default function DataTable({ data }: { data: DataType[] }) {
       size="small"
       pagination={{ pageSize: 5 }}
       className="w-full"
-      rootClassName='data-table'
+      rootClassName="data-table"
     />
   )
 }
